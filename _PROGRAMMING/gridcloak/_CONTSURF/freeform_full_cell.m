421ca6db4b15ef1316c3294fb7240a892864b168
Lukas Powalla <lukas.powalla@googlemail.com>
2016-03-07 20:32:24 +0100

    did some stuf on the plane

--- _PROGRAMMING/gridcloak/_CONTSURF/freeform_full_cell.m
+++ _PROGRAMMING/gridcloak/_CONTSURF/freeform_full_cell.m
@@ -1,20 +1,18 @@
 % This script constructs the full cell with given Parameter set 
 %% Parameterset
-
-
 xlim=500;
 ylim=50;
-nx=50;
-ny=5;
-rx1=50;                                  
+nx=30;
+ny=3;
+rx1=50;
 rx2=500;
-ry1=5;            
+ry1=5;
 ry2=50;
 n=1.5;
-a=1;
-b=0;
-y0x=50;
-y0y=5;
+a=1.9;
+b=2.0;
+y0x=75;
+y0y=70;
 %% Constructing of Surface
 [X0,Y0] = meshgrid(linspace(0,xlim,nx),linspace(0,ylim,ny));
 Z00=-sqrt(a*freeform(rx1,rx2,y0x,X0).^2+b*freeform(ry1,ry2,y0y,Y0).^2);
@@ -26,14 +24,14 @@ beta=0;
 plotSurface(X,Y,Z0,alpha,beta,'xlim',xlim,'ylim',ylim);
 %display(relativeImprovement(X,Y,Z0,alpha1,beta1,'xlim',xlim,'ylim',ylim));
 %% annual intensity
-nalpha=20;
-nbeta=20;
-[alphas,betas] = meshgrid(linspace(0,80,nalpha),linspace(0,80,nbeta));
-P_values=zeros(nalpha,nbeta);
-for i=1:nalpha
-    for k=1:nalpha
-    P_values(i,k)=relativeImprovement(X,Y,Z0,alphas(1,i),betas(k,1))+1;
-    end
-end
-
-Q = annualImprovement(alphas,betas,P_values)
+% nalpha=40;
+% nbeta=40;
+% [alphas,betas] = meshgrid(linspace(0,80,nalpha),linspace(0,80,nbeta));
+% P_values=zeros(nalpha,nbeta);
+% for i=1:nalpha
+%     for k=1:nalpha
+%     P_values(i,k)=relativeImprovement(X,Y,Z0,alphas(1,i),betas(k,1))+1;
+%     end
+% end
+% 
+% Q = annualImprovement(alphas,betas,P_values)
